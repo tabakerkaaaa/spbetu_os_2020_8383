eb7445d9b215b3044bcc26816d0592476f5a87b5
Michael <jakutenshi@gmail.com>
2020-02-17 11:48:03 +0300

    Create fetching scripts

---
+++ remotes.sh
@@ -0,0 +1,20 @@
+git remote add origin           git@github.com:JAkutenshi/spbetu_os_2020_8383.git
+# git remote add olyaave          https://github.com/olyaave/spbetu_os_2020_8383.git          # Аверина
+# git remote add tabakerkaaaa     https://github.com/tabakerkaaaa/spbetu_os_2020_8383.git     # Бабенко
+# git remote add GlebDot          https://github.com/GlebDot/spbetu_os_2020_8383.git          # Бессуднов
+# git remote add holyunchained    https://github.com/holyunchained/spbetu_os_2020_8383.git    # Дейнега
+# git remote add MilaIshanina     https://github.com/MilaIshanina/spbetu_os_2020_8383.git     # Ишанина
+git remote add tsvlad           https://github.com/tsvlad/spbetu_os_2020_8383.git           # Колмыков
+# git remote add Pchooo           https://github.com/Pchooo/spbetu_os_2020_8383.git           # Кормщикова
+# git remote add kikos2359        https://github.com/kikos2359/spbetu_os_2020_8383.git        # Костарев
+# git remote add ???              https://github.com//spbetu_os_2020_8383.git                 # Курбанов
+# git remote add TxAnton          https://github.com/TxAnton/spbetu_os_2020_8383.git          # Ларин
+# git remote add AlexxxMaxxx      https://github.com/AlexxxMaxxx/spbetu_os_2020_8383.git      # Максимов
+# git remote add MadAndrei        https://github.com/MadAndrei/spbetu_os_2020_8383.git        # Мирсков
+# git remote add Kirill-mol       https://github.com/Kirill-mol/spbetu_os_2020_8383.git       # Мололкин
+# git remote add VladisStep       https://github.com/VladisStep/spbetu_os_2020_8383.git       # Степанов
+# git remote add KaterinaSyrtsova https://github.com/KaterinaSyrtsova/spbetu_os_2020_8383.git # Сырцова
+# git remote add GeorgeTim        https://github.com/GeorgeTim/spbetu_os_2020_8383.git        # Тимченко
+# git remote add IlyaFedorov115   https://github.com/IlyaFedorov115/spbetu_os_2020_8383.git   # Федоров
+# git remote add IvanShish        https://github.com/IvanShish/spbetu_os_2020_8383.git        # Шишкин
+# git remote add honorhunter      https://github.com/honorhunter/spbetu_os_2020_8383.git      # Яхимович
