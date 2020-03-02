660f9a8ec80716464cdc06f9f886c3da46677c2f
Michael <jakutenshi@gmail.com>
2020-03-02 10:54:19 +0300

    unlock a number of students in scripts

--- fetch.sh
+++ fetch.sh
@@ -5,14 +5,14 @@
 # git fetch MilaIshanina
 git fetch tsvlad
 # git fetch Pchooo
-# git fetch kikos2359
+git fetch kikos2359
 git fetch TxAnton
 # git fetch AlexxxMaxxx
 git fetch MadAndrei
-# git fetch Kirill-mol
-# git fetch VladisStep
+git fetch Kirill-mol
+git fetch VladisStep
 # git fetch KaterinaSyrtsova
 # git fetch GeorgeTim
-# git fetch IlyaFedorov115
-# git fetch IvanShish
+git fetch IlyaFedorov115
+git fetch IvanShish
 # git fetch honorhunter
--- remotes.sh
+++ remotes.sh
@@ -6,15 +6,15 @@ git remote add origin           git@github.com:JAkutenshi/spbetu_os_2020_8383.gi
 # git remote add MilaIshanina     https://github.com/MilaIshanina/spbetu_os_2020_8383.git     # Ишанина
 git remote add tsvlad           https://github.com/tsvlad/spbetu_os_2020_8383.git           # Колмыков
 # git remote add Pchooo           https://github.com/Pchooo/spbetu_os_2020_8383.git           # Кормщикова
-# git remote add kikos2359        https://github.com/kikos2359/spbetu_os_2020_8383.git        # Костарев
+git remote add kikos2359        https://github.com/kikos2359/spbetu_os_2020_8383.git        # Костарев
 # git remote add ???              https://github.com//spbetu_os_2020_8383.git                 # Курбанов
  git remote add TxAnton          https://github.com/TxAnton/spbetu_os_2020_8383.git          # Ларин
 # git remote add AlexxxMaxxx      https://github.com/AlexxxMaxxx/spbetu_os_2020_8383.git      # Максимов
  git remote add MadAndrei        https://github.com/MadAndrei/spbetu_os_2020_8383.git        # Мирсков
-# git remote add Kirill-mol       https://github.com/Kirill-mol/spbetu_os_2020_8383.git       # Мололкин
-# git remote add VladisStep       https://github.com/VladisStep/spbetu_os_2020_8383.git       # Степанов
+git remote add Kirill-mol       https://github.com/Kirill-mol/spbetu_os_2020_8383.git       # Мололкин
+git remote add VladisStep       https://github.com/VladisStep/spbetu_os_2020_8383.git       # Степанов
 # git remote add KaterinaSyrtsova https://github.com/KaterinaSyrtsova/spbetu_os_2020_8383.git # Сырцова
 # git remote add GeorgeTim        https://github.com/GeorgeTim/spbetu_os_2020_8383.git        # Тимченко
-# git remote add IlyaFedorov115   https://github.com/IlyaFedorov115/spbetu_os_2020_8383.git   # Федоров
-# git remote add IvanShish        https://github.com/IvanShish/spbetu_os_2020_8383.git        # Шишкин
+git remote add IlyaFedorov115   https://github.com/IlyaFedorov115/spbetu_os_2020_8383.git   # Федоров
+git remote add IvanShish        https://github.com/IvanShish/spbetu_os_2020_8383.git        # Шишкин
 # git remote add honorhunter      https://github.com/honorhunter/spbetu_os_2020_8383.git      # Яхимович
